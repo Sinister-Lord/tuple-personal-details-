ced62b47f319df90bdd807c77ab746eca1fb0e0b
Sinister-Lord <80533597+Sinister-Lord@users.noreply.github.com>
2025-05-22 10:20:04 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Step 1: Store personal details in a tuple
+personal_details = ("Harsh", "Sharma", 18, 5.9, 65, "Mathematics")
+
+# Step 2: Print the original tuple
+print("Original Tuple:")
+print(personal_details)
+
+# Step 3: Convert the tuple to a list using list()
+personal_details_list = list(personal_details)
+
+# Step 4: Print the converted list
+print("\nConverted List:")
+print(personal_details_list)
